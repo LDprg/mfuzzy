c5bf2738edd6191b9ac56c91ac32ba52a71b5a27
LDprg <lukas_4dr@gmx.at>
2025-03-06 15:59:13 +0100

    Very basic Levensthein Alogrithm

--- build.zig
+++ build.zig
@@ -4,16 +4,11 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    const opts = .{ .target = target, .optimize = optimize };
+    const zbench_module = b.dependency("zbench", opts).module("zbench");
 
     const exe = b.addExecutable(.{
         .name = "mfuzzy",
@@ -21,6 +16,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("zbench", zbench_module);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,54 +1,160 @@
 const std = @import("std");
+const zbench = @import("zbench");
+
+const data = @import("data.zig");
+
+const test_data = data.test2;
+
+const lev_single_fn = fn (alloc: ?std.mem.Allocator, str1: []const u8, str2: []const u8) usize;
+// const lev_multiple_fn = fn (alloc: ?std.mem.Allocator, data: [][2][]const u8) usize;
+
+const LevSingleBench = struct {
+    fun: *const lev_single_fn,
+
+    fn init(fun: *const lev_single_fn) LevSingleBench {
+        return .{ .fun = fun };
+    }
+
+    pub fn run(self: LevSingleBench, alloc: std.mem.Allocator) void {
+        for (test_data) |item| {
+            std.mem.doNotOptimizeAway(self.fun(alloc, item[0], item[1]));
+        }
+    }
+};
+
+// const LevMultipleBench = struct {
+//     fun: *const lev_multiple_fn,
+//
+//     fn init(fun: *const lev_multiple_fn) LevMultipleBench {
+//         return .{ .fun = fun };
+//     }
+//
+//     pub fn run(self: LevMultipleBench, alloc: std.mem.Allocator) void {
+//         std.mem.doNotOptimizeAway(self.fun(alloc, test_data));
+//     }
+// };
 
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator();
-    // defer {
-    //     const deinit_status = gpa.deinit();
-    //     if (deinit_status == .leak) @panic("Memory leaked!");
-    // }
+    const stdout = std.io.getStdOut().writer();
+    var bench = zbench.Benchmark.init(std.heap.page_allocator, .{});
+    defer bench.deinit();
 
-    var items = [_][]const u8{"kitten"};
+    try bench.addParam("Levenshtein single recursive", &LevSingleBench.init(&levSingleRec), .{});
+    try bench.addParam("Levenshtein single full matrix", &LevSingleBench.init(&levSingleFullMatrix), .{});
+    try bench.addParam("Levenshtein single part matrix", &LevSingleBench.init(&levSinglePartMatrix), .{});
 
-    const search = "sitting";
-
-    fuzzy(@constCast(&items), search);
+    try stdout.writeAll("\n");
+    try bench.run(stdout);
 }
 
-pub fn fuzzy(items: [][]const u8, search: []const u8) void {
-    std.debug.print("Search for {s} in: ", .{search});
-    for (items) |str|
-        std.debug.print("\"{s}\" ", .{str});
-    std.debug.print("\n\n", .{});
+test "Lev Single Recursive" {
+    const res = levSingleRec(null, "kitten", "sitting");
+    errdefer std.debug.panic("Res: {}", .{res});
 
-    var bestDistance: i32 = std.math.maxInt(i32);
-    var bestString: []const u8 = "";
+    try std.testing.expect(res == 3);
+}
 
-    for (items) |str| {
-        const dist = levenshteinDistanceRecursive(search, str);
+fn levSingleRec(_: ?std.mem.Allocator, str1: []const u8, str2: []const u8) usize {
+    if (str1.len == 0)
+        return str2.len;
 
-        if (dist < bestDistance) {
-            bestString = str;
-            bestDistance = dist;
+    if (str2.len == 0)
+        return str1.len;
+
+    if (str1[0] == str2[0])
+        return levSingleRec(null, str1[1..], str2[1..]);
+
+    return 1 + @min(
+        levSingleRec(null, str1[1..], str2),
+        levSingleRec(null, str1, str2[1..]),
+        levSingleRec(null, str1[1..], str2[1..]),
+    );
+}
+
+test "Lev Single Full Matrix" {
+    const alloc = std.testing.allocator;
+
+    const res = levSingleFullMatrix(alloc, "kitten", "sitting");
+    errdefer std.debug.panic("Res: {}", .{res});
+
+    try std.testing.expect(res == 3);
+}
+
+fn levSingleFullMatrix(_alloc: ?std.mem.Allocator, str1: []const u8, str2: []const u8) usize {
+    var arena = std.heap.ArenaAllocator.init(_alloc.?);
+    defer arena.deinit();
+
+    const alloc = arena.allocator();
+
+    var mat = alloc.alloc([]usize, str2.len) catch std.debug.panic("Out of memory!", .{});
+    for (mat) |*arr| {
+        arr.* = alloc.alloc(usize, str1.len) catch std.debug.panic("Out of memory!", .{});
+        @memset(arr.*, 0);
+    }
+
+    for (mat, 1..) |*item, i| {
+        item.*[0] = i;
+    }
+
+    for (mat[0], 1..) |*item, i| {
+        item.* = i;
+    }
+
+    for (1..str1.len) |ind1| {
+        for (1..str2.len) |ind2| {
+            var subcost: usize = 1;
+            if (str1[ind1] != str2[ind2]) {
+                subcost = 0;
+            }
+
+            mat[ind2][ind1] = @min(
+                mat[ind2 - 1][ind1] + 1,
+                mat[ind2][ind1 - 1] + 1,
+                mat[ind2 - 1][ind1 - 1] + subcost,
+            );
         }
     }
 
-    std.debug.print("Best for \"{s}\" is \"{s}\" with distance {}\n", .{ search, bestString, bestDistance });
+    return mat[str2.len - 1][str1.len - 1];
 }
 
-pub fn levenshteinDistanceRecursive(str1: []const u8, str2: []const u8) i32 {
-    if (str1.len == 0)
-        return @intCast(str2.len);
+test "Lev Single Part Matrix" {
+    const alloc = std.testing.allocator;
 
-    if (str2.len == 0)
-        return @intCast(str1.len);
+    const res = levSinglePartMatrix(alloc, "kitten", "sitting");
+    errdefer std.debug.panic("Res: {}", .{res});
 
-    if (str1[0] == str2[0])
-        return levenshteinDistanceRecursive(str1[1..], str2[1..]);
+    try std.testing.expect(res == 3);
+}
 
-    return 1 + @min(
-        levenshteinDistanceRecursive(str1[1..], str2),
-        levenshteinDistanceRecursive(str1, str2[1..]),
-        levenshteinDistanceRecursive(str1[1..], str2[1..]),
-    );
+fn levSinglePartMatrix(_alloc: ?std.mem.Allocator, str1: []const u8, str2: []const u8) usize {
+    var arena = std.heap.ArenaAllocator.init(_alloc.?);
+    defer arena.deinit();
+
+    const alloc = arena.allocator();
+
+    var v0: []usize = alloc.alloc(usize, str2.len) catch std.debug.panic("Out of memory", .{});
+    var v1: []usize = alloc.alloc(usize, str2.len) catch std.debug.panic("Out of memory", .{});
+
+    for (v0, 0..) |*item, idx|
+        item.* = idx;
+
+    for (0..str1.len - 1) |i| {
+        v1[0] = i;
+
+        for (0..str2.len - 1) |j| {
+            const delCost = v0[j + 1] + 1;
+            const insCost = v1[j] + 1;
+
+            const subCost = if (str1[i] == str2[j]) v0[j] else v0[j] + 1;
+
+            v1[j + 1] = @min(delCost, insCost, subCost);
+        }
+
+        const swap = v0;
+        v0 = v1;
+        v1 = swap;
+    }
+
+    return v0[v0.len - 1];
 }
